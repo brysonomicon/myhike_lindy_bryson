f3e7d559420fcd1d9eafcf478e7989f077eeafab
Bryson Lindy <bslindy@gmail.com>
2024-02-20 15:30:59 -0800

    Changed contents in accordance with class guide

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -1,14 +1,15 @@
 //----------------------------------------
 //  Your web app's Firebase configuration
 //----------------------------------------
-var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyB-5ksYOQQ80gTqkarbpHA4Ddeq_kMgzzs",
+    authDomain: "comp-1800demo-320f3.firebaseapp.com",
+    projectId: "comp-1800demo-320f3",
+    storageBucket: "comp-1800demo-320f3.appspot.com",
+    messagingSenderId: "135465756811",
+    appId: "1:135465756811:web:486f2c140ffbbb3a460c8e"
+  };
+  
 
 //--------------------------------------------
 // initialize the Firebase app
